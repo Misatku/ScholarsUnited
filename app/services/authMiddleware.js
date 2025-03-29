96c1c563630b2e34671af4bf6e586e1c7501a338
Michaela <radostom@roehampton.ac.uk>
2025-03-29 12:40:11 +0000

    login disabled

--- app/services/authMiddleware.js
+++ app/services/authMiddleware.js
@@ -1,4 +1,6 @@
 function ensureAuthenticated(req, res, next) {
+  /*
+  //TEMP DISABLED FOR DESIGN WORK
     if (req.session.user) {
       // If the User is authenticated, proceed to the next middleware or route handler
       return next();
@@ -6,6 +8,8 @@ function ensureAuthenticated(req, res, next) {
   
     // If the User is not authenticated, redirect to the login page
     res.redirect("/login");
+    */
+   return next();
   }
   
   module.exports = ensureAuthenticated;
