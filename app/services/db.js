1ab44df7af17fa1ea5050fcad9b9f5839813a7db
Michaela <radostom@roehampton.ac.uk>
2025-03-29 13:18:39 +0000

    db

--- app/services/db.js
+++ app/services/db.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
-
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST || 'db', // Use MYSQL_HOST, not DB_CONTAINER
-    port: process.env.DB_PORT || 3306,
-    user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'password',
-    database: process.env.MYSQL_DATABASE || 'StudyBuddies',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+  host: process.env.MYSQL_HOST || "db",
+  port: process.env.MYSQL_PORT || 3306,
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "password",
+  database: process.env.MYSQL_DATABASE || "StudyBuddies",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 module.exports = pool;
